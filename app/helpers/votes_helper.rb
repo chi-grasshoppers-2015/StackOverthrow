52293050e9ac16073fa52ecfe334495af5b0089c
Connor Ring & Jasmine Feldmann & Spencer Rohan <pair+ccring.jasmine-feldmann.spencerrohan@devbootcamp.com>
2015-08-06 11:10:21 -0500

    Add voting functionality for questions and answers

--- app/helpers/votes_helper.rb
+++ app/helpers/votes_helper.rb
@@ -1,9 +1,27 @@
 helpers do
   def match_voter_id(post)
-    post.votes.voter_id.include?(current_user.id)
+    votes = post.votes.map{|vote| vote.voter_id}
+    votes.include?(current_user.id)
   end
 
   def find_vote(post)
-    post.votes.find_by(voter_id: current_user.id)
+    matching_votes = post.votes.map do |vote|
+      if vote.voter_id == current_user.id
+        vote
+      end
+    end
+    matching_votes.compact!
+    p matching_votes
+    matching_votes[0].value if matching_votes[0]
+  end
+
+  def find_question_id(vote)
+    object_class = vote.votable_type
+    object = object_class.constantize.find(vote.votable_id)
+    if object_class == "Question"
+      object.id
+    elsif object_class == "Answer"
+      object.question_id
+    end
   end
 end
