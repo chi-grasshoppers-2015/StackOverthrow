962628c8b6845b4c2aea630cfbbff57bb13c4f78
Jasmine Feldmann & Matt Bogen <pair+jasmine-feldmann.zebogen@devbootcamp.com>
2015-08-06 15:49:04 -0500

    Add final edit functionality for edit answer drop down form

--- public/js/application.js
+++ public/js/application.js
@@ -1,34 +1,35 @@
 $(document).ready(function() {
-  $('.edit-answer-button > a').on("click", function(event) {
-    event.preventDefault();
-    var url = this.href;
-    var answerId = $(this).parents()[0].id;
-    var editFormRequest = $.ajax({
-      url: url,
-      method: "get"
-    });
-    editFormRequest.done(function(responseData) {
-      var editForm = $(responseData).find(".edit-answer-form");
-      var postBody = $('#body-' + answerId);
-      $(postBody).hide();
-      $($(postBody).parents()[0]).append(editForm);
-      $('.edit-answer-form').on('submit', function(event) {
-        event.preventDefault();
-        var url = $(this).find("form").attr("action");
-        var newData = $(this).find('textarea').val();
-        var editRequest = $.ajax({
-          url: url,
-          method: "put",
-          data: { answer: newData }
-        });
-        editRequest.done(function(response) {
-          $(postBody).find('span').text(newData);
-          $(editForm).remove();
-          $(postBody).show();
-        });
+  $('.edit-answer-button > a').on("click", handleEditRequest);
+
+});
+
+var handleEditRequest = function(event) {
+  event.preventDefault();
+  var url = this.href;
+  var answerId = $(this).parents()[0].id;
+  var editFormRequest = $.ajax({
+    url: url,
+    method: "get"
+  });
+  editFormRequest.done(function(responseData) {
+    var editForm = $(responseData).find(".edit-answer-form");
+    var postBody = $('#body-' + answerId);
+    $(postBody).hide();
+    $($(postBody).parents()[0]).prepend(editForm);
+    $('.edit-answer-form').on('submit', function(event) {
+      event.preventDefault();
+      var url = $(this).find("form").attr("action");
+      var newData = $(this).find('textarea').val();
+      var editRequest = $.ajax({
+        url: url,
+        method: "put",
+        data: { answer: newData }
+      });
+      editRequest.done(function(response) {
+        $(postBody).find('span').text(newData);
+        $(editForm).remove();
+        $(postBody).show();
       });
     });
   });
-
-
-});
+}
